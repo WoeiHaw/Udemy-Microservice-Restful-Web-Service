bafaee866888c7ff9d0a9978ecaf623e1f6f8c66
Ang <woeihaw94@gmail.com>
2026-03-04 07:57:41 +0800

    add code

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,8 +1,12 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import com.in28minutes.rest.webservices.UserNotFoundException;
 import com.in28minutes.rest.webservices.restful_web_services.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,11 +25,22 @@ public class UserResource {
 
     @GetMapping("/users/{userId}")
     public User retrieveUsers(@PathVariable Integer userId) {
-        return userDaoService.findOne(userId);
+        User user =userDaoService.findOne(userId);
+        if(user ==  null){
+            throw new UserNotFoundException("id:"+userId);
+        }
+
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user){
         userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+         return ResponseEntity.created(location).build();
     }
 }
