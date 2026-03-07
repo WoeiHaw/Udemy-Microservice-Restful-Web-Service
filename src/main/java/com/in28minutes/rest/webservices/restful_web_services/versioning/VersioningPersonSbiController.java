7d30fd218fb2c1309a5e7c8a1b8d6074d053e410
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-03-07 12:02:36 +0800

    Add versioning

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/VersioningPersonSbiController.java
@@ -0,0 +1,48 @@
+package com.in28minutes.rest.webservices.restful_web_services.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonSbiController {
+//
+//    @GetMapping(value = "/{version}/personsbi",version = "1.0.0")
+//    public PersonV1 getFirstVersionOfPerson(){
+//        return new PersonV1("Bod Charlie");
+//    }
+//
+//    @GetMapping(value = "/{version}/personsbi",version ="2.0.0")
+//    public PersonV2 getSecondVersionOfPerson(){
+//        return new PersonV2(new Name("Bob","Charlie"));
+//    }
+
+//    @GetMapping(path = "/personsbi",version = "1.0.0")
+//    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+//        return new PersonV1("Bob Charlie");
+//    }
+//
+//    @GetMapping(path = "/personsbi",version = "2.0.0")
+//    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+//        return new PersonV2(new Name("Bob","Charlie"));
+//    }
+
+//    @GetMapping(path = "/personsbi/header",headers = "X-API-VERSION=1")
+//    public PersonV1 getFirstVersionOfPersonRequestHeaders(){
+//        return new PersonV1("Bob Charlie");
+//    }
+//
+//    @GetMapping(path = "/personsbi/header",headers = "X-API-VERSION=2")
+//    public PersonV2 getSecondVersionOfPersonRequestHeaders(){
+//        return new PersonV2(new Name("Bob","Charlie"));
+//    }
+//
+//    @GetMapping(path = "/personsbi/accept",produces = "application/vnd.company.app-v1+json")
+//    public PersonV1 getFirstVersionOfPersonAcceptHeaders(){
+//        return new PersonV1("Bob Charlie");
+//    }
+//
+//    @GetMapping(path = "/personsbi/accept",produces = "application/vnd.company.app-v2+json")
+//    public PersonV2 getSecondVersionOfPersonAcceptHeaders(){
+//        return new PersonV2(new Name("Bob","Charlie"));
+//    }
+}
