d8555db2eb20c34834b75d462a9f8c4cb5260d5e
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-03-12 07:18:09 +0800

    Add mysql

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserJpaResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserJpaResource.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import com.in28minutes.rest.webservices.restful_web_services.jpa.PostRepository;
 import com.in28minutes.rest.webservices.restful_web_services.jpa.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
@@ -18,8 +19,11 @@ public class UserJpaResource {
 
     private final UserRepository userRepository;
 
-    public UserJpaResource(UserRepository userRepository) {
+    private final PostRepository postRepository;
+
+    public UserJpaResource(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     @GetMapping("/users")
@@ -56,5 +60,30 @@ public class UserJpaResource {
     public void deleteUser(@PathVariable Integer userId) {
         userRepository.deleteById(userId);
 
+    }
+
+    @GetMapping("/users/{id}/posts")
+    public List<Post> retrievePostForUsers(@PathVariable Integer id) {
+        User user =userRepository.findById(id).orElse(null);
+        if(user ==  null){
+            throw new UserNotFoundException("id:"+id);
+        }
+        return user.getPosts();
+    }
+
+    @PostMapping("/users/{id}/posts")
+    public ResponseEntity<Object> createPostForUser(@PathVariable Integer id,@Valid @RequestBody Post post) {
+        User user =userRepository.findById(id).orElse(null);
+        if(user ==  null){
+            throw new UserNotFoundException("id:"+id);
+        }
+       post.setUser(user);
+       Post savePost =  postRepository.save(post);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savePost.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,12 @@ values
 (10001,current_date(),'Range'),
 (10002,current_date(),'Ravi'),
 (10003,current_date(),'Sathish');
+
+insert into post(id,description,user_id)
+values
+(20001,'I want to learn AWS',10001),
+(20002,'I want to learn DevOps',10001),
+(20003,'I want to Get AWS Certified',10002),
+(20004,'I want to learn Multi Cloud',10002);
+
+
