7bebe33765ace9599213e69dc3a4b2cace75e23a
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-03-12 20:49:37 +0800

    Add spring security

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices.restful_web_services.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
+
+        httpSecurity.authorizeHttpRequests(
+                auth ->auth.anyRequest().authenticated()
+        );
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+}
